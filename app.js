a1fdbf4f375070debb59280d20a36902bbd3b8d1
Lv Shun <lvshun@tsintergy.com>
2023-11-03 09:26:09 +0800

    fix: eslint 问题修复

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import path from 'path'
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,11 +8,10 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 const app = express();
-app.set('path', __dirname)
-
+app.set('path', __dirname);
 
 export {
   __dirname,
-  router,
-}
+  router
+};
 export default app;
--- utils/interface/index.js
+++ utils/interface/index.js
@@ -5,19 +5,19 @@ const createInterface = ({
   methods,
   callback,
   header,
-  app,
+  app
 }) => {
   const interfaceChild = new InterfaceCreate({ methods, name, header });
   if (methods === undefined || methods === 'get') {
-    app.get(`/${name}`, function(req, res){
-      interfaceChild.callback(callback, req, res)
-    } )
+    app.get(`/${name}`, function(req, res) {
+      interfaceChild.callback(callback, req, res);
+    });
   } else if (methods === 'post') {
-    app.post(`/${name}`, function(req, res){
-      interfaceChild.callback(callback, req, res)
-    }) 
+    app.post(`/${name}`, function(req, res) {
+      interfaceChild.callback(callback, req, res);
+    });
   }
-  return interfaceChild
-}
+  return interfaceChild;
+};
 
-export default createInterface
+export default createInterface;
