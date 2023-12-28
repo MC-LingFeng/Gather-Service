420e3f387550a8b036fb69d6b0fc1088fd8c9247
Lv Shun <lvshun@tsintergy.com>
2023-12-28 14:03:06 +0800

    feat: 接入openai

--- src/interface/openai/index.ts
+++ src/interface/openai/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getMessage } from './utils';
+import { getMessage, getMessageTragic } from './utils';
 import { chatForMsg } from '@/OpenAI';
 
 const router = express.Router();
@@ -9,9 +9,28 @@ router.post('/setmessage', (req, res) => {
   const values = chatForMsg(createMessage as any);
 
   values.then((data) => {
-    res.json({ code: 200, message: 'success', data: { username: data }});
+    res.json({ code: 200, message: 'success', data: data?.choices?.map((item) => ({ ...item, message: {
+      role: item.message.role,
+      content: item.message.content?.replace(/\n/g, '<br/>') ?? ''
+    }})) ?? [] });
   }).catch((err) => {
     console.log(err, 'Error');
+    res.json({ code: 100, message: 'error', data: [] });
+  });
+});
+
+router.post('/setmessage/tragic', (req, res) => {
+  const createMessage = getMessageTragic(req.body);
+  const values = chatForMsg(createMessage as any);
+
+  values.then((data) => {
+    res.json({ code: 200, message: 'success', data: data?.choices?.map((item) => ({ ...item, message: {
+      role: item.message.role,
+      content: item.message.content?.replace(/\n/g, '<br/>') ?? ''
+    }})) ?? [] });
+  }).catch((err) => {
+    console.log(err, 'Error');
+    res.json({ code: 100, message: 'error', data: [] });
   });
 });
 
--- src/interface/openai/utils.ts
+++ src/interface/openai/utils.ts
@@ -1,6 +1,23 @@
 import { MessageBody } from './type';
 
 export const getMessage = (body: MessageBody) => {
-  const content = `作为一名周易算命大师，请根据我提供的信息帮我计算我未来的运势。我的名字叫${body.name}，我的年龄是${body.age}，我的性别是${body.gender}，我的生日是${body.birthday}，我最近发生的奇怪的事是${body.msg}`;
-  return [{ 'role': 'user', 'content': content, 'name': `${body.name}的运势` }];
+  const systemContent = '你是一个非常受欢迎的周易算命大师，你得算命水平很高，结合周易算命的逻辑，根据下面的人物信息算出他的命运趋势';
+  const userContent = `我的名字叫${body.name}，我的年龄是${body.age}，我的性别是${body.gender}，我的生日是${body.birthday}，我最近发生的奇怪的事是${body.msg}`;
+  const assistantContent = `结合六爻术数算出${body.name}的命劫、${body.name}的姻缘和运势，占卜出${body.name}发生的奇怪的事寓意着什么。`;
+  return [
+    { 'role': 'system', 'content': systemContent },
+    { 'role': 'user', 'content': userContent },
+    { 'role': 'assistant', 'content': assistantContent }
+  ];
+};
+
+export const getMessageTragic = (body: MessageBody) => {
+  const systemContent = '你是一个非常受欢迎的周易算命大师，你得算命水平很高，你算出的命都很凄惨，惨到让人流泪，并且你的回答充满了不情愿和讽刺';
+  const userContent = `我的名字叫${body.name}，我的年龄是${body.age}，我的性别是${body.gender}，我的生日是${body.birthday}，我最近发生的奇怪的事是${body.msg}`;
+  const assistantContent = `请不情愿和讽刺地写出${body.name}悲惨的命运`;
+  return [
+    { 'role': 'system', 'content': systemContent },
+    { 'role': 'user', 'content': userContent },
+    { 'role': 'assistant', 'content': assistantContent }
+  ];
 };
